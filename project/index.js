ef0b4c1f0e6f2731e985060ef15ff18ff4dd71b5
Pedro Martins <pmdmartins@gmail.com>
2021-01-25 22:33:43 +0000

    carregar foto

--- project/index.js
+++ project/index.js
@@ -9,14 +9,50 @@ function carregar(){
 	  	type:"get",
 	  	async: true,
        success : function(data, status, response) {
-        adicionarFotos();
+        adicionarFotos(data);
        }
    });    
   }
       
   
-function adicionarFotos() {
-     alert("Carreguei a pagina");
+function adicionarFotos(data) {
+     var arrayDeFotos = data;
+
+     for (var i=0; i<arrayDeFotos.length; i++) {
+      var foto = arrayDeFotos[i];
+      criarFoto(foto);
+}
+
+function criarFoto(foto) {
+
+  // criar h5
+  var h5 = document.createElement("h5");
+  h5.className = "card-title";
+  h5.innerText = foto.user.name;
+
+  // criar div filha
+  var div = document.createElement("div");
+  div.className = "card-body";
+  div.appendChild(h5);
+  
+  // criar img
+  var img = document.createElement("img");
+  img.className = "card-img-top";
+  var imgSrc = foto.urls.full;
+  img.setAttribute("src", imgSrc);
+
+  // criar div pai
+  var divPrincipal = document.createElement("div");
+  divPrincipal.className = "card col-3";
+  divPrincipal.appendChild(img);
+  divPrincipal.appendChild(div);
+
+  // adicionar div pai à pagina/DOM
+  var container = document.getElementById("contentorFilmes");
+  container.appendChild(divPrincipal);
+}
+
+
 }
 
    function programarCarregamentoPagina() {
